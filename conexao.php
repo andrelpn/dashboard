69e68142e83ca437003430cb891391f8b9a013bf
Andre <andre_lpn@hotmail.com>
2020-07-20 23:32:15 -0300

    Exemplos

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost"; // padrão - server local
+$database = "baseteste"; // alterar conforme o nome do seu banco de dados
+$username = "root"; //mysql - usuário padrão
+$password= ""; //myslq - senha do banco de dados
+$conexao = mysqli_connect($servername, $username, $password, $database);
+
+?>
---
+++ primeiroGrafico.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+         ['Cidade', 'População', { role: 'annotation'}],
+          <?php
+
+          include 'conexao.php';
+          $sql = "SELECT * FROM cidades";
+          $buscar = mysqli_query($conexao,$sql);
+          while ($dados = mysqli_fetch_array($buscar)){
+            $cidade = $dados['cidade'];
+            $populacao = $dados['populacao'];
+
+         
+          ?>
+ 
+         ['<?php echo $cidade ?>',<?php echo $populacao ?>,<?php echo $populacao ?>],
+
+<?php
+ } 
+ ?>
+
+      ]);
+
+        var options = {
+          title: 'População das Cidades',
+          //curveType: 'function',
+          legend: { position: 'top' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('graficoLinha'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="graficoLinha" style="height:400px"></div>
+  </body>
+</html>
